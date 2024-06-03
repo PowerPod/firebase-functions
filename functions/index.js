159be00e85de00a6d7df1ea06191a4a848e76f9b
ultra <>
2024-06-03 22:27:52 +0800

    add auth toke for api

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,20 @@ const { onDocumentCreated } = require('firebase-functions/v2/firestore')
 const { initializeApp } = require('firebase-admin/app')
 const { getFirestore } = require('firebase-admin/firestore')
 
+const { defineString } = require('firebase-functions/params')
+
+const AUTH_TOKEN = defineString('MINT_PT_AUTH_TOKEN')
+
 initializeApp()
 
 exports.mintPT = onRequest(async (req, res) => {
+  const authHeader = req.headers['authorization']
+
+  if (!authHeader || authHeader !== `Bearer ${AUTH_TOKEN.value()}`) {
+    res.status(403).json({ code: '3', message: 'forbidden' })
+    return
+  }
+
   const { orderId, address, amount } = req.body
 
   if (!orderId || !address || !amount) {
